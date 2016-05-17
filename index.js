70329c350c1be7156a32a3e897581d8662c305c4
hunterlester <guilfordhunter@gmail.com>
2016-05-17 13:05:38 -0700

    es6 exports

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+exports.cleanest = cleanest;
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-function flattenStructure(structure) {
+function cleanest(structure) {
   if (structure.__proto__ == Array.prototype) {
     var _ret = function () {
       var structureObj = {};
@@ -16,7 +22,7 @@ function flattenStructure(structure) {
       } else {
         structure.map(function (entry) {
           if (entry.__proto__ == Object.prototype) {
-            structureObj = Object.assign({}, structureObj, _defineProperty({}, entry._id, flattenStructure(entry)));
+            structureObj = Object.assign({}, structureObj, _defineProperty({}, entry._id, cleanest(entry)));
           } else {
             structureObj = Object.assign({}, structureObj, _defineProperty({}, entry, entry));
           }
@@ -35,7 +41,7 @@ function flattenStructure(structure) {
 
       Object.keys(structure).map(function (key) {
         if (structure[key].__proto__ == Array.prototype) {
-          structureObj = Object.assign({}, structure, structureObj, _defineProperty({}, key, flattenStructure(structure[key])));
+          structureObj = Object.assign({}, structure, structureObj, _defineProperty({}, key, cleanest(structure[key])));
         }
       });
       return {
--- src.js
+++ src.js
@@ -1,4 +1,4 @@
-function flattenStructure(structure) {
+export function cleanest(structure) {
     if(structure.__proto__ == Array.prototype) {
       let structureObj = {};
 
@@ -7,7 +7,7 @@ function flattenStructure(structure) {
       } else {
         structure.map(function(entry) {
             if(entry.__proto__ == Object.prototype) {
-              structureObj = Object.assign({}, structureObj, {[entry._id]: flattenStructure(entry)})
+              structureObj = Object.assign({}, structureObj, {[entry._id]: cleanest(entry)})
             } else {
               structureObj = Object.assign({}, structureObj, {[entry]: entry});
             }
@@ -21,7 +21,7 @@ function flattenStructure(structure) {
 
       Object.keys(structure).map(function(key) {
           if(structure[key].__proto__ == Array.prototype) {
-              structureObj = Object.assign({}, structure, structureObj, {[key]: flattenStructure(structure[key])});
+              structureObj = Object.assign({}, structure, structureObj, {[key]: cleanest(structure[key])});
           }
       });
       return structureObj;
